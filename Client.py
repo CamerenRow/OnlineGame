ae4d4c00750e526029cab776f15928f395e90ccc
CamerenRow <camerenrowlette2007@gmail.com>
2023-04-06 17:44:40 -0500

    Fixed a few bugs

--- Client.py
+++ Client.py
@@ -6,27 +6,39 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.196', port))
 print('Connected!')
 
+
 def CoinFlip():
-   while True:
-      print('heads(1) or tails(2)')
-      choice = int(input())
-   
+    while True:
+        print('heads(1) or tails(2)')
+        choice = str(input())
+        s.sendall(choice.encode())
+
+        result = s.recv(1024).decode('utf8')
+        print(result)
+
+
 def Recieve():
-   while True:
-      data = s.recv(1024).decode()
-      print(data)
-    
-t= threading.Thread(target=Recieve)
+    while True:
+        try:
+            data = s.recv(1024).decode()
+            print(data)
+            if data.startswith('/CF'):
+                s.send(data.encode())
+                t = threading.Thread(target=CoinFlip)
+                t.start()
+            break
+        except:
+            # server is probably closed
+            s.close()
+
+def send_message():
+    while True:
+      msg = input()
+      s.sendall(msg.encode())
+      break
+
+t = threading.Thread(target=Recieve)
 t.start()
 
-while True:
-   msg = input()
-
-   if msg.startswith('/CoinFlip'):
-        t= threading.Thread(target=CoinFlip)
-        t.start()
-        break
-   else:
-      s.sendall(msg.encode())
-
-
+send_thread = threading.Thread(target=send_message)
+send_thread.start()
--- Server.py
+++ Server.py
@@ -3,57 +3,73 @@ import threading
 import random
 port = 5555
 
+players = {}
+choices = {}
+player_id = 1
 
 Clients = []
 
+
+def Handle_Client(client_socket, client_address, player_id, players, choices):
+    while True:
+        if client_socket.recv(1024).decode():
+
+            if client_socket.recv(1024).decode().isdigit():
+                choice = int(client_socket.recv(1024).decode())
+
+                print(choice)
+                if choice not in [1, 2]:
+                    client_socket.sendall(
+                        'Invalid choice, please choose heads(1) or tails(2)'.encode())
+                continue
+            choices[player_id] = choice
+            if len(choices) == 2:
+                print('here')
+                player1_choice = choices[1]
+                player2_choice = choices[2]
+
+                result = random.randint(1, 2)
+                print(f'the result is {result}')
+
+                if(player1_choice == player2_choice):
+                    guessed_result = "It's a tie"
+                elif player1_choice == result:
+                    guessed_result = "player 1 wins"
+                else:
+                    guessed_result = "player 2 wins"
+
+                players[1].sendall(guessed_result.encode())
+                players[2].sendall(guessed_result.encode())
+
+                # reset choices dictionary
+                choices.clear()
+
+
 def Chat_Room(client_socket, client_address):
-   print(f'Connected to {client_address}')
-   while True:
-      msg = client_socket.recv(1024).decode()
+    print(f'Connected to {client_address}')
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode()
+            if not msg:
+                Clients.remove(client_socket)
+                print(f"Client disconnected: {client_address}")
+                break
+        except:
+            print(f'Client {client_address} disconected')
+            Clients.remove(client_socket)
+            client_socket.close()
+            break
 
-      if not msg:
-         socket_server.close()
-         client_socket.close()
-
-
-      if msg.startswith('/CoinFlip'):
-            t = threading.Thread(target=Handle_Client, args=(client_socket, client_address, player_id, players, choices))
+        print(msg)
+        if msg.startswith('/CF'):
+            t = threading.Thread(target=Handle_Client, args=(
+                client_socket, client_address, player_id, players, choices))
             t.start()
 
-      for client in Clients:
-         if client != client_socket:
-           client.sendall(msg.encode())
-      
-def Handle_Client(client_socket, client_address, player_id, players, choices):
-    client_socket.sendall(f"Welcome, Player {player_id}!\n".encode())
-
-    while True:
-       choice = int(client_socket.recv(1024).decode().strip().lower())
-       print(choice)
-
-       if choice not in [1, 2]:
-           client_socket.sendall('Invalid choice, please choose heads(1) or tails(2)'.encode())
-           continue
-       choices[player_id] = choice
-       if len(choices) == 2:
-        player1_choice = choices[1]
-        player2_choice = choices[2]
-
-        result = random.randint(1, 2)
-        print(f'the result is {result}')
-
-        if(player1_choice == player2_choice):
-           guessed_result = "It's a tie"
-        elif player1_choice == result:
-           guessed_result = "player 1 wins"
-        else:
-           guessed_result = "player 2 wins"
-        
-        players[1].sendall(guessed_result.encode())
-        players[2].sendall(guessed_result.encode())
-
-        # reset choices dictionary
-        choices.clear()
+        for client in Clients:
+            if client != client_socket:
+                client.sendall(msg.encode())
+                
 
 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -61,11 +77,6 @@ socket_server.bind(('192.168.1.196', port))
 
 socket_server.listen(2)
 print('looking for connections')
-
-players = {}
-choices = {}
-player_id = 1
-
 
 
 while True:
@@ -75,7 +86,7 @@ while True:
 
     players[player_id] = client_socket
 
-    t =  threading.Thread(target=Chat_Room, args=(client_socket, client_address))
+    t = threading.Thread(target=Chat_Room, args=(
+        client_socket, client_address))
     t.start()
     player_id += 1
-
